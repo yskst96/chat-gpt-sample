3240c67b7ce69146a098242e5ec83b23d69f4614
yusukesata <yskstdev@gmail.com>
2023-04-12 11:56:25 +0900

    initその2

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="h-full bg-slate-800">
+    <div class="container">
+      <div>You:hello</div>
+      <div>Agent:Hi</div>
+    </div>
   </main>
 </template>
